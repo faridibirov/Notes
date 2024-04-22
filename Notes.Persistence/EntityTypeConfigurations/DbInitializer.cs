0a4f136d29ed06634adb29aee43f8e4a52e39b4d
Farid Dibirov <farid962@gmail.com>
2024-04-22 16:18:33 +0400

    first commit

---
+++ Notes.Persistence/EntityTypeConfigurations/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence.EntityTypeConfigurations;
+
+public class DbInitializer
+{
+    public static void Initialize (NotesDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
