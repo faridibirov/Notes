ade7a33c1c3103b5f3c32c7e6d39aa41933cbd98
Farid Dibirov <farid962@gmail.com>
2024-05-20 16:45:56 +0400

    FluentValidation

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using MediatR;
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 
 namespace Notes.Application;
 
@@ -9,6 +11,11 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
 		services.AddMediatR(Assembly.GetExecutingAssembly());
+        services
+            .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
 
 		return services;
     }
---
+++ Notes.Application/Notes/Commands/UpdateNotes/UpdateNoteCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Commands.UpdateNotes;
+
+public class UpdateNoteCommandValidator : AbstractValidator<UpdateNoteCommand>
+{
+
+    public UpdateNoteCommandValidator()
+    {
+        RuleFor(updateNoteCommand=> updateNoteCommand.UserId).NotEqual(Guid.Empty);
+        RuleFor(updateNoteCommand=> updateNoteCommand.Id).NotEqual(Guid.Empty);
+        RuleFor(updateNoteCommand => updateNoteCommand.Title)
+            .NotEmpty().MaximumLength(250);
+}
