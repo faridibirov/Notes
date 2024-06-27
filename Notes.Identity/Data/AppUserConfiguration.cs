9a0fe9db46ca672818b3eb1c7959f2c1a7ae68a4
Farid Dibirov <farid962@gmail.com>
2024-06-27 11:58:31 +0400

    IdentityServer4 part2

--- Notes.Identity/Data/AppUserConfiguration.cs
+++ Notes.Identity/Data/AppUserConfiguration.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Identity.Models;
 
 namespace Notes.Identity.Data;
 
-public class AppUserConfiguration : IEntityTypeConfiguration<AppUserConfiguration>
+public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
 {
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
 }
--- Notes.Identity/Program.cs
+++ Notes.Identity/Program.cs
@@ -1,24 +1,67 @@
 using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Notes.Identity;
 using Notes.Identity.Data;
+using Notes.Identity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+}).AddEntityFrameworkStores<AuthDbContext>().AddDefaultTokenProviders();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
     .AddInMemoryApiResources(Configuration.ApiResources)
     .AddInMemoryIdentityResources(Configuration.IdentityResource)
     .AddInMemoryApiScopes(Configuration.ApiScopes)
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
 
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
 var app = builder.Build();
 
 app.UseIdentityServer();
 
+SeedDatabase();
+
 app.MapGet("/", () => "Hello World!");
 
+
 app.Run();
+
+void SeedDatabase()
+
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var serviceProvider = scope.ServiceProvider;
+
+        try
+        {
+            var context = serviceProvider.GetRequiredService<AuthDbContext>();
+            DbInitializer.Initialize(context);
+        }
+        
+        catch (Exception ex)
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occured while app initialization");
+        }
+    }
+}
