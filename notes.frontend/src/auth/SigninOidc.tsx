27f41292d1e33d9a7510d30527ae35231bc30075
Farid Dibirov <farid962@gmail.com>
2024-07-05 17:32:07 +0400

    react app

---
+++ notes.frontend/src/auth/SigninOidc.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, FC} from 'react';
+import { useNavigate } from "react-router-dom";
+import {signinRedirectCallback} from './user-service';
+
+const SigninOidc: FC<{}> = () => {
+
+    const navigate = useNavigate();
+    useEffect(()=>  {
+        async function signinAsync() {
+            await signinRedirectCallback();
+            navigate('/');
+        }
+        signinAsync();
+    }, [history]);
+    return <div>Redirecting...</div>;
+};
+
+export default SigninOidc;
--- notes.frontend/src/notes/NotesList.tsx
+++ notes.frontend/src/notes/NotesList.tsx
@@ -2,4 +2,52 @@ import {FC, ReactElement, useRef, useEffect, useState} from 'react';
 import {CreateNoteDto, Client, NoteLookupDTO} from '../api/api';
 import { FormControl } from 'react-bootstrap';
 
-const apiClient = new Client('https://localhost:44353')
+const apiClient = new Client('https://localhost:44353');
+
+async function CreateNote(note:CreateNoteDto) {
+    await apiClient.create('1.0', note);
+    console.log('Note is created.')
+}
+
+const NoteList: FC<{}> = () : ReactElement => {
+    let textInput = useRef(null);
+    const [notes, setNotes] = useState<NoteLookupDTO[] | undefined>(undefined);
+
+    async function getNotes() {
+        const noteListVm = await apiClient.getAll('1.0');
+        setNotes(noteListVm.notes);
+    }
+
+    useEffect(()=> {
+        getNotes();
+    }, []);
+
+    const handleKeyPress = (event:React.KeyboardEvent<HTMLInputElement>) => {
+
+        if (event.key==='Enter') {
+            const note: CreateNoteDto = {
+                title: event.currentTarget.value
+            };
+            CreateNote(note);
+            event.currentTarget.value = '';
+            getNotes();
+        }
+    };
+
+return(
+    <div>
+        Notes
+        <div>
+            <FormControl ref={textInput} onKeyPress = {handleKeyPress} ></FormControl>
+        </div>
+        <div>
+            {notes?.map(note=>(
+                <div>{note.title}</div>
+            ))}
+        </div>
+    </div>
+);
+
+};
+
+export default NoteList;
